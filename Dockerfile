31b1517506f2c1d1c7e7599c28b7c25295aed068
Komal <komalgangwar16@gmail.com>
2025-04-10 10:36:42 +0530

    Removed previous files

--- Dockerfile
+++
@@ -1,5 +0,0 @@
-# Dockerfile
-FROM php:8.1-apache
-COPY public/ /var/www/html/
-RUN docker-php-ext-install mysqli
-EXPOSE 80
--- backend/conn.php
+++
@@ -1,6 +0,0 @@
-<?php
-$conn=new mysqli("localhost","root","","vrinda");
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-?>
